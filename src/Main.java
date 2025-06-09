c145629e1da2c3c740ce27b2438268ceecb8f52b
Matt Bennett <144359294+filmteacher@users.noreply.github.com>
2025-06-08 22:16:02 -0400

    Finished assignment

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,126 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-    }
-}
+public class Main
+{
+    public static void main(String[] args)
+    {
+        boolean endIt = false;
+        do
+        {
+            String playerA = "";
+            String playerB = "";
+            String input = "";
+            String trash = "";
+            boolean aDone = false;
+            boolean bDone = false;
+            boolean againDone = false;
+
+            do
+            {
+                System.out.print("Player A, enter your move - rock, paper, or scissors [RPS]: ");
+                Scanner in = new Scanner(System.in);
+                input = in.nextLine();
+                input = input.toUpperCase();
+
+                if(input.equals("R") || input.equals("P") || input.equals("S"))
+                {
+                    playerA = input;
+                    aDone = true;
+                }
+                else
+                {
+                    // Not a double so use nextLine() instead to read a String
+                    trash = input;
+                    System.out.println("Player A, you said your move was: " + trash);
+                    System.out.println("You have to enter a valid move [RPS]!");
+                }
+            }while(!aDone);
+
+            do
+            {
+                System.out.print("Player B, enter your move - rock, paper, or scissors [RPS]: ");
+                Scanner in = new Scanner(System.in);
+                input = in.nextLine();
+                input = input.toUpperCase();
+
+                if(input.equals("R") || input.equals("P") || input.equals("S"))
+                {
+                    playerB = input;
+                    bDone = true;
+                }
+                else
+                {
+                    // Not a double so use nextLine() instead to read a String
+                    trash = input;
+                    System.out.println("Player B, you said your move was: " + trash);
+                    System.out.println("You have to enter a valid move [RPS]!");
+                }
+            }while(!bDone);
+
+            System.out.println("Player A, your move was: " + playerA);
+            System.out.println("Player B, your move was: " + playerB);
+
+            switch (playerA)
+            {
+                case "R":
+                    switch (playerB)
+                    {
+                        case "R":
+                            System.out.println("Rock vs Rock. It’s a TIE!");
+                            break;
+                        case "P":
+                            System.out.println("Paper covers Rock. Player B WINS!");
+                            break;
+                        default:
+                            System.out.println("Rock breaks Scissors. Player A WINS!");
+                    }
+                    break;
+                case "P":
+                    switch (playerB)
+                    {
+                        case "R":
+                            System.out.println("Paper covers Rock. Player A WINS!");
+                            break;
+                        case "P":
+                            System.out.println("Paper vs. Paper. It's a TIE!");
+                            break;
+                        default:
+                            System.out.println("Scissors cut Paper. Player B WINS!");
+                    }
+                    break;
+                default:
+                    switch (playerB)
+                    {
+                        case "R":
+                            System.out.println("Rock breaks Scissors. Player B WINS!");
+                            break;
+                        case "P":
+                            System.out.println("Scissors cut Paper. Player A WINS!");
+                            break;
+                        default:
+                            System.out.println("Scissors vs. Scissors. It's a TIE!");
+                    }
+                    break;
+            }
+
+            do {
+                System.out.print("Would you like to play again? [Y/N]: ");
+                Scanner in = new Scanner(System.in);
+                input = in.nextLine();
+                input = input.toUpperCase();
+
+                if (input.equals("Y"))
+                {
+                    againDone = true;
+                } else if (input.equals("N"))
+                {
+                    endIt = true;
+                    againDone = true;
+                } else {
+                    trash = input;
+                    System.out.println("You have to enter Y or N!");
+                }
+            }while(!againDone);
+        }while(!endIt);
+    };
+};
